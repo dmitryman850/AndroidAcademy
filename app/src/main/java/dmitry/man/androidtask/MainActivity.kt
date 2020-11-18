2d760f38d839ad19216e8d2e3957d535ba258e43
Dmitry Maniuk <dmitryman850@gmail.com>
2020-11-18 20:51:54 +0300

    add movie_details_activity

--- app/src/main/java/dmitry/man/androidtask/MainActivity.kt
+++ app/src/main/java/dmitry/man/androidtask/MainActivity.kt
@@ -1,11 +1,33 @@
 package dmitry.man.androidtask
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mBtnToMovieDetails: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mBtnToMovieDetails = findViewById(R.id.btn_to_movie_details_activity)
+
+        mBtnToMovieDetails.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view) {
+            mBtnToMovieDetails -> {
+                toMovieDetails()
+            }
+        }
+    }
+
+    private fun toMovieDetails() {
+        startActivity(Intent(this, MovieDetailsActivity::class.java))
     }
 }
---
+++ app/src/main/java/dmitry/man/androidtask/MovieDetailsActivity.kt
@@ -0,0 +1,15 @@
+package dmitry.man.androidtask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+
+class MovieDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+
+    }
+}
