7d5f621ea642ed7351bc44fbbb37e6ffed69008e
Dmitry Maniuk <dmitryman850@gmail.com>
2020-12-18 18:30:31 +0300

    add json

---
+++ app/src/main/java/dmitry/man/androidtask/data/Genre.kt
@@ -0,0 +1,3 @@
+package dmitry.man.androidtask.data
+
+data class Genre(val id: Int, val name: String)
---
+++ app/src/main/java/dmitry/man/androidtask/data/Movie.kt
@@ -0,0 +1,18 @@
+package dmitry.man.androidtask.data
+
+import dmitry.man.androidtask.data.Actor
+import dmitry.man.androidtask.data.Genre
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val poster: String,
+    val backdrop: String,
+    val ratings: Float,
+    val numberOfRatings: Int,
+    val minimumAge: Int,
+    val runtime: Int,
+    val genres: List<Genre>,
+    val actors: List<Actor>
+)
